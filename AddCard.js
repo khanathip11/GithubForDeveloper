ae2c03852f29742ba58ee4f515cb771fb3a263e7
khanathip11 <160195943+khanathip11@users.noreply.github.com>
2024-11-08 16:32:47 +0700

    Revert "test project"
    This reverts commit 2d00ed56dfd3f51a5155c1d8528f06eec4988fc1.

--- AddCard.js
+++ AddCard.js
@@ -1,19 +1,3 @@
 console.log('Add your Card Please!');
 
-alert('Test Server!');
-
-const inputElement = document.querySelector('.input');
-const buttonElement = document.querySelector('.button');
-const paraElement = document.querySelector('.paragraft');
-
-buttonElement.addEventListener('click' ,onClick);
-
-function onClick() {
-    console.log('Compleate!')
-    paraElement.innerHTML = '';
-    const result = inputElement.value * 3 / 4;
-    if(result) {
-        paraElement.innerHTML = result;
-    };
-    inputElement.value = 0;
-};
+alert('banana');
