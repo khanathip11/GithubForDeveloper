e5a9412c5bb1425c514dd79553755ee53510cd62
khanathip11 <160195943+khanathip11@users.noreply.github.com>
2024-11-29 13:31:35 +0700

    function to do list
    update Function

--- AddCard.js
+++ AddCard.js
@@ -1,13 +1,87 @@
 const arrays = [];
+const btn = document.querySelector(".btnAdd");
 
+btn.addEventListener("click", onClick);
 
+function onClick(event) {
+  event.preventDefault();
+  addTask();
+  console.log(event);
+}
 
+function addTask() {
+  const inputElem = document.querySelector(".input");
+  const text = inputElem.value.trim();
+  arrays.push({ text: text, complete: false });
+  inputElem.value = '';
+  updateTask();
+  statusTask();
+  console.log(arrays);
+}
+
+function updateTask() {
+  const ulElem = document.querySelector(".ul");
+  ulElem.innerHTML = "";
+  arrays.forEach((task, index) => {
+    const liElem = document.createElement("li");
+    liElem.innerHTML = `
+        <div class="todo_input_section task ${task.complete ? 'complete' : ''}">
+            <input type="checkbox" ${task.complete ? 'checked' : ''}>
+                <span>${task.text}</span>
+        </div>
+        <div class="todo_img_section">
+            <span class="editTodolist" onClick="editTask(${index})">
+                <img src="/edit_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png">
+            </span>
+            <span class="deleteTodolist" onClick="deleteTask(${index})">
+                <img src="/delete_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24 (1).png" alt="">
+            </span>
+        </div>
+        `;
+        console.log(liElem)
+        liElem.addEventListener('change',() => onChang(index));
+        ulElem.append(liElem);
+    });
+}
+
+function onChang(index) {
+    arrays[index].complete = !arrays[index].complete;
+    console.log({arrays});
+    updateTask();
+    statusTask();
+};
+
+function editTask(index) {
+    const inputElem = document.querySelector('.input');
+    inputElem.value = arrays[index].text;
+    arrays.splice(index, 1);
+    updateTask();
+    statusTask();
+    console.log(arrays)
+};
+
+function deleteTask(index) {
+    arrays.splice(index , 1);
+    updateTask();
+    statusTask();
+    console.log(arrays)
+};
+
+function statusTask() {
+    const completeTask = arrays.filter((task) => task.complete).length
+    const totalTask = arrays.length;
+    const processTask = (completeTask / totalTask) * 100;
+    const progressbar = document.querySelector('.todo_stat');
+    progressbar.style.width = `${processTask}%`;
+    const scroll = document.querySelector('.number');
+    scroll.innerText = `${completeTask} / ${totalTask}`
+};
 
 
 // const express = require('express')
 // const app = express();
 
-// Route 2 
+// Route 2
 // const productAPI = require('.Files/product')
 
 // Route 3
@@ -21,51 +95,13 @@ const arrays = [];
 // Route 2
 // app.use('/api', require('api', file));
 
-// Route 3 
+// Route 3
 // app.readdirSync('.Files').map((r) => app.use('/api/' ,require('./Files/' + r)));
 
 // Port
 // app.listen(3000, () => {
 //     console.log('Hello Server')
 // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const array = [];
 // const btnAdd = document.querySelector(".btnAdd");
